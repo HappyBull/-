5886718cedd54947e06dde28e8cd65920940dd71
zouz <964444396@qq.com>
2022-07-03 19:44:26 +0800

    2022/07/03 每日一题

---
+++ leetcode/src/dayExercises/Question556.java
@@ -0,0 +1,47 @@
+package dayExercises;
+
+/**
+ * @author zouzh
+ * @date 2022/7/3 19:35
+ */
+
+public class Question556 {
+    public int nextGreaterElement(int n) {
+        char[] nums = Integer.toString(n).toCharArray();
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i < 0) {
+            return -1;
+        }
+
+        int j = nums.length - 1;
+        while (j >= 0 && nums[i] >= nums[j]) {
+            j--;
+        }
+        swap(nums, i, j);
+        reverse(nums, i + 1);
+        long ans = Long.parseLong(new String(nums));
+        return ans > Integer.MAX_VALUE ? -1 : (int) ans;
+    }
+
+    public void reverse(char[] nums, int begin) {
+        int i = begin, j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    public void swap(char[] nums, int i, int j) {
+        char temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    public static void main(String[] args) {
+        Question556 q = new Question556();
+        System.out.println(q.nextGreaterElement(12385764));
+    }
+}
