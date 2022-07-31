886568eed6634542dc7695f7a0d92f4079cf4164
zouz <964444396@qq.com>
2022-07-31 19:05:40 +0800

    2022/07/31 每日一题

---
+++ leetcode/src/dayExercises/Question1161.java
@@ -0,0 +1,41 @@
+package dayExercises;
+
+import common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author zouzh
+ * @date 2022/7/31 19:04
+ */
+
+public class Question1161 {
+
+    private List<Integer> sum = new ArrayList<Integer>();
+
+    public int maxLevelSum(TreeNode root) {
+        dfs(root, 0);
+        int ans = 0;
+        for (int i = 0; i < sum.size(); ++i) {
+            if (sum.get(i) > sum.get(ans)) {
+                ans = i;
+            }
+        }
+        return ans + 1; // 层号从 1 开始
+    }
+
+    private void dfs(TreeNode node, int level) {
+        if (level == sum.size()) {
+            sum.add(node.val);
+        } else {
+            sum.set(level, sum.get(level) + node.val);
+        }
+        if (node.left != null) {
+            dfs(node.left, level + 1);
+        }
+        if (node.right != null) {
+            dfs(node.right, level + 1);
+        }
+    }
+}
