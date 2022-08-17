07f19662f3f6d6e9aa18e693491c9d491d5665f0
zouz <964444396@qq.com>
2022-08-17 21:57:03 +0800

    2022/08/17 每日一题

---
+++ leetcode/src/dayExercises/Question1422.java
@@ -0,0 +1,37 @@
+package dayExercises;
+
+import java.util.Arrays;
+
+/**
+ * @author zouzh
+ * @date 2022/8/14 17:50
+ */
+
+public class Question1422 {
+    public int maxScore(String s) {
+        int score = 0;
+        int n = s.length();
+        if (s.charAt(0) == '0') {
+            score++;
+        }
+        for (int i = 1; i < n; i++) {
+            if (s.charAt(i) == '1') {
+                score++;
+            }
+        }
+        int ans = score;
+        for (int i = 1; i < n - 1; i++) {
+            if (s.charAt(i) == '0') {
+                score++;
+            } else {
+                score--;
+            }
+            ans = Math.max(ans, score);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
